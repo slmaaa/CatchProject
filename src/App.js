5e92361087921af43ad58fc187a991766f48ce5a
slmaaa <maszelong2001@gmail.com>
2021-03-31 22:37:08 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import 'react-native-gesture-handler';
-import * as React from 'react';
-import { View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import "react-native-gesture-handler";
+import * as React from "react";
+import { View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import InGame from './components/InGame';
+import InGame from "./components/InGame";
+import Map from "./components/Map";
 
 const Stack = createStackNavigator();
 
-const App = props => {
+const App = (props) => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="InGame">
         <Stack.Screen
           name="InGame"
           component={InGame}
+          options={{
+            headerLeft: null,
+            title: null,
+            headerStyle: {
+              backgroundColor: "black",
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Map"
+          component={Map}
           options={{
             headerLeft: null,
             title: null,
