7dbaefd2f0bd51be527007bfb4bc696f1ce6e091
815147-1115147 <jluiaa@connect.ust.hk>
2021-04-23 22:30:35 +0800

    Update LoadingHome.js

--- src/components/LoadingHome.js
+++ src/components/LoadingHome.js
@@ -26,16 +26,12 @@ export default LoadingHome = ({ navigation }) => {
       });
   }
   async function setLocal() {
-    console.log(userID, userName);
     await MMKV.setStringAsync("userID", userID);
     await MMKV.setStringAsync("userName", userName);
-    if (userStatus === "OFFLINE") {
-      userStatus = "ONLINE";
-      database().ref(`users/${userID}`).update({ status: "ONLINE" });
-    }
+    if (userStatus === "OFFLINE") userStatus = "ONLINE";
     await MMKV.setStringAsync("userStatus", userStatus);
     if (gameID != null) {
-      MMKV.setInt("userGameID", gameID);
+      MMKV.setString("userGameID", gameID);
     }
     await navigation.replace("Home");
   }
@@ -43,6 +39,7 @@ export default LoadingHome = ({ navigation }) => {
     await getDataFromDB();
   };
   temp();
+
   return (
     <SafeAreaView>
       <Text>Loading</Text>
