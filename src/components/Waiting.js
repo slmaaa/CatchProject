a039fe3474231e5b5b8dedfe23408e69a46e8a96
815147-1115147 <jluiaa@connect.ust.hk>
2021-05-08 06:45:17 +0800

    update styles in waiting.js

--- src/components/Waiting.js
+++ src/components/Waiting.js
@@ -1,6 +1,6 @@
 /* eslint-disable quotes */
 import React, { useState, useEffect } from "react";
-import { SafeAreaView, Text, ScrollView, StyleSheet, View } from "react-native";
+import { SafeAreaView, Text, ScrollView, StyleSheet, View, Image, StatusBar, Dimensions } from "react-native";
 import { Avatar, Button } from "react-native-elements";
 import database from "@react-native-firebase/database";
 import MMKVStorage from "react-native-mmkv-storage";
@@ -10,6 +10,9 @@ import { deleteGame, getGame } from "./Helper/server";
 import useInterval from "./Helper/useInterval";
 import { wsSend } from "../App";
 import Icon from "react-native-vector-icons";
+
+var { height, width } = Dimensions.get('window');
+var link = "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/7bbe5762c79ee0ad11c1267483b4a2d5e12868de779eaf751e8e86596e978bbb._V_SX1080_.jpg";
 
 export default Waiting = ({ navigation }) => {
   const MMKV = new MMKVStorage.Loader().initialize();
@@ -75,33 +78,14 @@ export default Waiting = ({ navigation }) => {
       list.push(
         <View style={styles.playerListRowConatiner} key={i % 2}>
           <View style={styles.leftPlayer} key={i}>
-            <Text style={styles.LplayerName} key={i}>
-                {playerList[i]}
+            <Text style={styles.LplayerName}>
+              {playerList[i]}
             </Text>
-            <Avatar
-              rounded
-              source={{
-                uri:"https://images-na.ssl-images-amazon.com/images/S/pv-target-images/7bbe5762c79ee0ad11c1267483b4a2d5e12868de779eaf751e8e86596e978bbb._V_SX1080_.jpg",
-              }}
-              //icon={{name: 'home'}}
-            />
-            {/* <Icon
-                reverse
-                name="sc-telegram"
-                type="evilicon"
-                size={20}
-                color="white"
-            /> */}
+            <Image style={styles.cardImage} source={{ uri: link }} />
           </View>
           <View style={styles.rightPlayer} key={i + 1}>
-            <Avatar
-              rounded
-              source={{
-                uri:"https://images-na.ssl-images-amazon.com/images/S/pv-target-images/7bbe5762c79ee0ad11c1267483b4a2d5e12868de779eaf751e8e86596e978bbb._V_SX1080_.jpg",
-              }}
-              //icon={{name: 'home'}}
-            />
-            <Text style={styles.RplayerName} key={i + 1}>
+            <Image style={styles.cardImage} source={{ uri: link }} />
+            <Text style={styles.RplayerName}>
               {playerList[i + 1]}
             </Text>
           </View>
@@ -116,11 +100,11 @@ export default Waiting = ({ navigation }) => {
               {playerList[i]}
             </Text>
             <Avatar
-                rounded
-                source={{
-                    uri:"https://images-na.ssl-images-amazon.com/images/S/pv-target-images/7bbe5762c79ee0ad11c1267483b4a2d5e12868de779eaf751e8e86596e978bbb._V_SX1080_.jpg",
-                }}
-                //icon={{name: 'home'}}
+              rounded
+              source={{
+                uri: "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/7bbe5762c79ee0ad11c1267483b4a2d5e12868de779eaf751e8e86596e978bbb._V_SX1080_.jpg",
+              }}
+            //icon={{name: 'home'}}
             />
           </View>
         </View>
@@ -140,22 +124,17 @@ export default Waiting = ({ navigation }) => {
   };
 
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={styles.container}>
       <View style={styles.headerContainer}>
         <Text style={styles.headerText}>
           {`${game.hostName}'s Room`}
           {`\nRoom ID: ${game.gid}`}
         </Text>
-        <Avatar
-          rounded
-          //style={{alignSelf: "center"}}
-          source={{
-              uri:"https://images-na.ssl-images-amazon.com/images/S/pv-target-images/7bbe5762c79ee0ad11c1267483b4a2d5e12868de779eaf751e8e86596e978bbb._V_SX1080_.jpg",
-          }}
-          //icon={{name: 'home'}}
-        />
+        <Image style={styles.cardImage} source={{ uri: link }} />
       </View>
-      <ScrollView style={styles.playersListContainer}>{playerView}</ScrollView>
+      <View style={styles.playersListContainer}>
+        <ScrollView>{playerView}</ScrollView>
+      </View>
       <Button
         title={"Confirm"}
         containerStyle={styles.button}
@@ -165,7 +144,7 @@ export default Waiting = ({ navigation }) => {
           wsSend(JSON.stringify({ header: "START", content: gameID }));
         }}
       ></Button>
-    </SafeAreaView>
+    </View>
   );
 };
 const styles = StyleSheet.create({
@@ -174,45 +153,77 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   headerContainer: {
-    marginTop: "10%",
-    marginBottom: "20%",
+    width: "45%",
     height: "10%",
+    flexDirection: 'row',
+    marginTop: height * 0.05,
+    marginBottom: 30,
+    borderBottomRightRadius: height / 20,
+    borderTopRightRadius: height / 20,
     backgroundColor: "#00000080",
-    width: "48%",
-    borderTopRightRadius: 50,
-    borderBottomRightRadius: 50,
-    alignContent: "center",
-    justifyContent: "center",
-    flexDirection: "row",
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
   headerText: {
-    fontSize: 14,
-    fontWeight: "700",
-    width:"90%",
-    color: "#FFFFFFFF",
-    textAlign: "center",
-    textAlignVertical: "center",
-    paddingLeft:"10%",
+    fontSize: 10,
+    color: "#FFFFFF",
+    fontWeight: 'normal',
+    paddingLeft: 10,
+    textAlign: "left",
+  },
+  cardImage: {
+    borderRadius: height / 30,
+    height: height / 15,
+    width: height / 15,
+    marginHorizontal: height / 50,
+  },
+  playersListContainer: { height: "60%" },
+  playerListRowConatiner: {
+    height: 100,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 5,
+  },
+  leftPlayer: {
+    width: width * 0.40,
+    height: height * 0.1,
+    flexDirection: 'row',
+    marginTop: 10,
+    borderBottomRightRadius: height * 0.05,
+    borderTopRightRadius: height * 0.05,
+    backgroundColor: "#00000080",
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  rightPlayer: {
+    width: width * 0.40,
+    height: height * 0.1,
+    flexDirection: 'row',
+    marginTop: 10,
+    borderTopLeftRadius: height * 0.05,
+    borderBottomLeftRadius: height * 0.05,
+    backgroundColor: "#00000080",
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
   LplayerName: {
     fontSize: 14,
     fontWeight: "700",
-    width: "70%",
     color: "white",
     textAlign: "center",
     textAlignVertical: "center",
-    paddingLeft:"10%",
+    flex: 3,
+    paddingLeft: 20,
   },
   RplayerName: {
     fontSize: 14,
     fontWeight: "700",
-    width: "70%",
     color: "white",
     textAlign: "center",
     textAlignVertical: "center",
-    paddingRight: 5,
+    marginLeft: -40,
+    flex: 3,
   },
-  playersListContainer: { height: "60%" },
   button: {
     height: 50,
     width: "50%",
@@ -228,33 +239,5 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.37,
     shadowRadius: 15,
     elevation: 5,
-  },
-  playerListRowConatiner: {
-    height: 100,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginBottom: 5,
-  },
-  leftPlayer: {
-    //flex: 0.36,
-    flexDirection: "row",
-    width: "40%",
-    height: "100%",
-    borderTopRightRadius: 50,
-    borderBottomRightRadius: 50,
-    backgroundColor: "#00000080",
-    alignContent: "center",
-    justifyContent: "center",
-  },
-  rightPlayer: {
-    //flex: 0.36,
-    flexDirection: "row",
-    width: "40%",
-    height: "100%",
-    borderTopLeftRadius: 50,
-    borderBottomLeftRadius: 50,
-    backgroundColor: "#00000080",
-    alignContent: "center",
-    justifyContent: "center",
   },
 });
