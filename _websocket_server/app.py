8948501100b7d41746505247b0adceeea4342d4c
slmaaa <maszelong2001@gmail.com>
2021-05-13 00:10:28 +0800

    update

--- _websocket_server/app.py
+++ _websocket_server/app.py
@@ -201,8 +201,8 @@ async def player_stats_handler(websocket, _dict):
         await websocket.send('{"header": "ERROR", "content": "Save game fail"}')
     try:
         if flag:
-            game = gdb[_dict["gid"]]
             [distMVP, pointMVP] = game.findMVPs()
+            game = gdb[_dict["gid"]]
             await broadcast(_dict["gid"], "ACCOUNT_FINISHED", {"players": game["players"], "winTeam": game["winTeam"], "distMVP": distMVP, "pointMVP": pointMVP})
     except Exception as e:
         print(e)
